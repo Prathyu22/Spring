dc216e06941b8a407b36b704f4e2dfac65c49575
Prathyu22 <lakshmiprathyushaveturi@gmail.com>
2023-01-26 20:35:56 +0530

    usage of xml file

--- App.java
+++ App.java
@@ -1,11 +1,29 @@
 package com.navin.Telusko;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Car car = new Car();
-        car.drive();
+    	// Here we have 2 classes. They are Car and Bike.
+    	// In order to call the method of any class 
+    	// we need to change the source code 
+    	// that is we need to change the object mentioned in the source code. 
+        
+    	//Here comes the Dependency Injection (DI).
+    	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    	
+    	//getBean() is a method of ApplicationContext and Beanfactory Interfaces.
+    	//In general, getBean() gives us the object so that we type cast it to Vehicle here.
+    	
+    	
+    	//When we use .xml files, there is no need to recompile the java file. 
+    	// Only we can change the requirements in the .xml file. 
+    	Vehicle obj = (Vehicle) context.getBean("vehicle"); //new Car();
+        obj.drive();
+        
+        
     }
 }
